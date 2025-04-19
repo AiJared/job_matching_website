3ae1701618c9f021bf25cde7433ad71e46ee70ed
AiJared <ojared511@gmail.com>
2025-04-19 14:25:41 +0300

    updating project level urls with accounts urls

--- job_matching/urls.py
+++ job_matching/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('', RedirectView.as_view(url='accounts/login/', permanent=False)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
