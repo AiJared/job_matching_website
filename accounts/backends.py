a7f7bea387ad2b7dbd6aa10189d90eb8f042d189
AiJared <ojared511@gmail.com>
2025-04-19 14:55:58 +0300

    supporting email backends

---
+++ accounts/backends.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+User = get_user_model()
+
+class EmailOrUsernameModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            # Try to find a user matching the username
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            try:
+                # Or try to find a user matching the email
+                user = User.objects.get(email=username)
+            except User.DoesNotExist:
+                # No user was found matching either username or email
+                return None
+        
+        # Now check the password
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
