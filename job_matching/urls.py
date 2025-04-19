334ba35faf3fdb45b6d6ebb838fd61a1e95da429
AiJared <ojared511@gmail.com>
2025-04-20 01:50:22 +0300

    configured dashboards to project urls

--- job_matching/urls.py
+++ job_matching/urls.py
@@ -7,6 +7,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('dashboards/', include('dashboards.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
