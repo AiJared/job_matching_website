1874380f6c63064332a2ed7bce06996b8c653831
AiJared <ojared511@gmail.com>
2025-04-24 15:58:05 +0300

    registering signals

--- candidate/apps.py
+++ candidate/apps.py
@@ -1,6 +1,10 @@
+# candidate/apps.py
 from django.apps import AppConfig
-
 
 class CandidateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'candidate'
+    verbose_name = 'Job Seeker Dashboard'
+    
+    def ready(self):
+        import candidate.signals  # Import signals to register them
